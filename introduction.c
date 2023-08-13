b12ef5b9fbc6d049fa18ae6562b04cbf9845ca7d
vde-frei <vde-frei@student.42sp.org.br>
2023-08-12 23:38:43 -0300

    fix: updated file with test and 'a.out'  Changes to be committed: 	modified:   introduction.c 	new file:   introduction_output

--- introduction.c
+++ introduction.c
@@ -6,7 +6,7 @@
 /*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/12 21:24:46 by vde-frei          #+#    #+#             */
-/*   Updated: 2023/08/12 21:41:08 by vde-frei         ###   ########.fr       */
+/*   Updated: 2023/08/12 23:34:57 by vde-frei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@
 /**
  * put information, like elements of an array
  * Don't matter if is a array of char or int
- * you can put both type in a struct. (line x)
- * */
+ * you can put both type in a struct.
+ */
 
 /**
  * After set all variables (int, char, size_t, etc...)
@@ -35,16 +35,28 @@
  *	Node *next;
  * };
  *
- * this way only change the way how you write it, but both have same result.
- * (start at line x)
- * */
+ * this only change the way how you write it, but both have same result.
+ */
 typedef struct s_Node
 {
 	int			x;
-	struct Node	*next;
+	struct s_Node	*next;
 }	t_Node;
 
 int	main(int argc, char *argv[])
 {
-	
+	/* start to define our list */
+	t_Node	root;
+	/* create a second to use as an address in 'next' */
+	t_Node	elem2;
+	root.x = 15;
+	/* now call 'next' and give a NULL pointer or an address of another Node. */
+	root.next = &elem2;
+	/* now the same behavior of root will be repeated. */
+	elem2.x = -2;
+	/* note that here we don't want more 'Nodes' in next, so we can assign 'NULL' to *.next' */
+	elem2.next = NULL;
+	printf("First: %d\n", root.x);
+	printf("First: %d\n", elem2.x);
+	return (0);
 }
